912565ae424636cdb38889512908a2f6ffe48530
akashkuchankar <kuchankarakash@gmail.com>
2022-10-05 01:12:53 -0700

    Add project files.

---
+++ print even number by using for loop/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace print_even_number_by_using_for_loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           for(int i = 20; i <=100; i++)
+                if(i % 2 == 0)
+                    Console.WriteLine(i);
+            Console.ReadLine();
+            
+        }
+    }
+}
